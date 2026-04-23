a0ffe0c46066a7cc1cc1f8ad798e74425a9f2ff6
Yahinniel Vasquez <yahinnieltheking01@gmail.com>
2026-04-23 19:37:30 -0400

    init

---
+++ app.js
@@ -0,0 +1,121 @@
+const express = require('express')
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
+const { title } = require('node:process')
+const z = require('zod')
+const { movieSchema, validateMovie, validatePartialMovie } = require('./schemas/movies')
+
+const app = express()
+
+const ACEPTED_ORIGINS = ['http://127.0.0.1:5501']
+
+app.disable('x-powered-by')
+app.use(express.json())
+app.use((req, res, next) => {
+    const origin = req.headers.origin
+    if (ACEPTED_ORIGINS.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin)
+    }
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    if (req.method === 'OPTIONS') return res.sendStatus(200)
+    next()
+})
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'hola mundo'})
+})
+
+
+//GET /movies?genre=terror
+app.get('/movies', (req, res) => {
+    const {genre} = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()))
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
+})
+
+
+//GET /movies/:id
+app.get('/movies/:id', (req, res) => {
+    const {id} = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (!movie) {
+        return res.status(404).json({message: 'movie not found'})
+    }
+    res.json(movie)
+})
+
+
+//POST /movies
+app.post('/movies', (req, res) => {
+    const movie = validateMovie(req.body)
+
+    if (movie.error) {
+        return res.status(422)
+        .json({message: 'invalid movie data', errors: JSON.stringify(movie.error.errors)})
+    }
+
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...movie.data
+    }
+    
+    movies.push(newMovie)
+
+    const movieCreated = movies.find(m => m.id === newMovie.id)
+    if (movieCreated) {
+        res.status(201).json({message: 'movie created'})
+    } else {
+        res.status(500).json({message: 'error creating movie'})
+    }
+})
+
+
+//PATCH /movies/:id
+app.patch('/movies/:id', (req, res) => {
+    const {id} = req.params
+
+    const result = validatePartialMovie(req.body)
+
+    if (result.error) {
+        return res.status(422)
+        .json({message: 'invalid movie data', errors: JSON.stringify(result.error.errors)})
+    }
+
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({message: 'movie not found'})
+    }
+
+    movies[movieIndex] = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    res.json({message: 'movie updated'})
+})
+
+//DELETE /movies/:id
+app.delete('/movies/:id', (req, res) => {
+    const {id} = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({message: 'movie not found'})
+    }
+
+    movies.splice(movieIndex, 1)
+    res.json({message: 'movie deleted'})
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+})
+
---
+++ schemas/movies.js
@@ -0,0 +1,50 @@
+const { z } = require('zod')
+
+const movieSchema = z.object({
+    title: z.string(
+        invalid_type_error='title must be a string',
+        required_error='title is required'
+    ),
+
+    genre: z.array(z.enum(['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi', 'Romance'], {
+        invalid_type_error: 'genre must be an array of strings',
+        required_error: 'genre is required'
+    })),
+
+    year: z.number().int()
+    .min(1888, 'year must be greater than or equal to 1888')
+    .max(new Date().getFullYear(), `year must be less than or equal to ${new Date().getFullYear()}`),
+
+    director: z.string().optional(),
+
+    duration: z.number().int().positive('duration must be a positive integer').optional(),
+
+    rate: z.number()
+    .min(0, 'rate must be greater than or equal to 0')
+    .max(10, 'rate must be less than or equal to 10').optional(),
+
+    poster: z.string().url('poster must be a valid URL').optional()
+})
+
+function validateMovie(movie) {
+    try {
+       return movieSchema.safeParse(movie)
+    } catch (error) {
+        return { valid: false, errors: error.errors }
+    }
+}
+
+function validatePartialMovie(params) {
+    const partialSchema = movieSchema.partial()
+    try {
+        return partialSchema.safeParse(params)
+    } catch (error) {
+        return { valid: false, errors: error.errors }
+    }
+}
+
+module.exports = {
+    movieSchema,
+    validateMovie,
+    validatePartialMovie
+}
